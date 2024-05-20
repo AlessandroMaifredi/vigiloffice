83edfe575051bfc76b04827f633ec1ea635af536
q.toska <q.toska@campus.unimib.it>
2024-05-20 17:30:59 +0200

    Flag per retained message su welcome settato a true
    è stata provata la comunicazione con un dispositivo slave stupido, che ascolta sul topic welcome, e funziona

---
+++ nodoMaster/secrets.h
@@ -0,0 +1,4 @@
+#define MQTT_BROKERIP "149.132.178.179"         // IP address of the machine running the MQTT broker
+#define MQTT_CLIENTID "vigilOffice-nodoMaster"                 // client identifier
+#define MQTT_USERNAME "QazimToska"         // mqtt user's name
+#define MQTT_PASSWORD "iot847361"         // mqtt user's password
