b1c2fce31917441297e411244973426dd5966347
q.toska <q.toska@campus.unimib.it>
2024-05-20 20:00:52 +0200

    semplicissima implementazione di un nodo slave per testing

--- nodoVentilazione/secrets.h
+++ nodoVentilazione/secrets.h
@@ -1,4 +1,4 @@
 #define MQTT_BROKERIP "149.132.178.179"         // IP address of the machine running the MQTT broker
-#define MQTT_CLIENTID "nodoMaster-**MAC**"                 // client identifier
+#define MQTT_CLIENTID "nodoVentilazione-**MAC**"                 // client identifier
 #define MQTT_USERNAME "QazimToska"         // mqtt user's name
 #define MQTT_PASSWORD "iot847361"         // mqtt user's password
