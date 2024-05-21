a75e3f125a40c94dbb42f62e37ab619567e968b1
Alessandro Maifredi <a.maifredi@campus.unimib.it>
2024-05-21 18:24:24 +0200

    Motion detection

---
+++ nodoLampadina/comm.cpp
@@ -0,0 +1,53 @@
+#include <DNSServer.h>
+#include <ESP8266WiFi.h>
+#include <WiFiManager.h>
+#include <MQTT.h>
+#include <ArduinoJson.h>
+#include "secrets.h"
+
+// MQTT data
+#define MQTT_BUFFER_SIZE 128               // the maximum size for packets being published and received
+MQTTClient mqttClient(MQTT_BUFFER_SIZE);   // handles the MQTT communication protocol
+WiFiClient networkClient;                  // handles the network connection to the MQTT broker
+#define MQTT_TOPIC_WELCOME "vigiloffice/welcome"
+#define AP_NAME "VigilOffice-Lampadina"
+
+void connectToMQTTBroker() {
+  if (!mqttClient.connected()) {   // not connected
+    Serial.print(F("\nConnecting to MQTT broker..."));
+    while (!mqttClient.connect(MQTT_CLIENTID, MQTT_USERNAME, MQTT_PASSWORD)) {
+      Serial.print(F("."));
+      delay(1000);
+    }
+    Serial.println(F("\nConnected!"));
+
+    // connected to broker, subscribe topics
+    mqttClient.subscribe(MQTT_TOPIC_WELCOME);
+    Serial.println(F("\nSubscribed to welcome topic!"));
+  }
+}
+
+void mqttMessageReceived(String &topic, String &payload) {
+  // this function handles a message from the MQTT broker
+  Serial.println("Incoming MQTT message: " + topic + " - " + payload);
+  if (topic == MQTT_TOPIC_WELCOME) {
+    JsonDocument doc;
+    deserializeJson(doc, payload);
+    const char *serverIP = doc["serverIP"];
+    Serial.println(serverIP);
+  }
+}
+
+void commSetup() {
+  mqttClient.begin(MQTT_BROKERIP, 1883, networkClient);   // setup communication with MQTT broker
+  mqttClient.onMessage(mqttMessageReceived);              // callback on message received from MQTT broker
+  WiFiManager wifiManager;
+  //wifiManager.resetSettings();
+  wifiManager.autoConnect(AP_NAME);
+}
+
+void commLoop() {
+  connectToMQTTBroker();   // connect to MQTT broker (if not already connected)
+  mqttClient.loop();       // MQTT client loop
+}
+
