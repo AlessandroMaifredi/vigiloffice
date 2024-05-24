a83adbb8ceb492b39502cd3f11360bdf2b259aaa
Alessandro Maifredi <a.maifredi@campus.unimib.it>
2024-05-24 17:05:40 +0200

    single file, status message

--- nodoLampadina/nodoLampadina.ino
+++ nodoLampadina/nodoLampadina.ino
@@ -1,5 +1,3 @@
-
-#include "comm.h"
 #include <ArduinoJson.h>
 
 // === ALARM ===
@@ -168,33 +166,46 @@ void readFlame() {
 #define RGB_GREEN_PIN D1
 #define RGB_BLUE_PIN D3
 
-enum RGB_STATUS {
-  RGB_ENABLED,
-  RGB_DISABLED,
+enum RGB_SENSOR_STATUS {
+  RGB_SENSOR_ENABLED,
+  RGB_SENSOR_DISABLED,
 };
 
-RGB_STATUS rgbStatus = RGB_ENABLED;
+enum RGB_STATUS {
+  RGB_ON,
+  RGB_OFF,
+  RGB_ALARM,
+  RGB_CUSTOM
+};
+
+RGB_SENSOR_STATUS rgbSensorStatus = RGB_SENSOR_ENABLED;
+
+RGB_STATUS rgbStatus = RGB_OFF;
 
 void setRGB(byte red, byte green, byte blue) {
   digitalWrite(RGB_RED_PIN, red);
   digitalWrite(RGB_GREEN_PIN, green);
   digitalWrite(RGB_BLUE_PIN, blue);
+  rgbStatus = RGB_CUSTOM;
 }
 
 void setRGBOff() {
   setRGB(LOW, LOW, LOW);
+  rgbStatus = RGB_OFF;
 }
 
 void setRGBOn() {
   setRGB(HIGH, HIGH, HIGH);
+  rgbStatus = RGB_ON;
 }
 
 void setRGBAlarm() {
   setRGB(HIGH, LOW, LOW);
+  rgbStatus = RGB_ALARM;
 }
 
 void updateRGB() {
-  if (rgbStatus == RGB_ENABLED) {
+  if (rgbSensorStatus == RGB_SENSOR_ENABLED) {
     if (alarmStatus == ALARM_ACTIVE) {
       setRGBAlarm();
     } else {
@@ -208,6 +219,129 @@ void updateRGB() {
       }
     }
   }
+}
+
+#include "wiring_private.h"
+#include <DNSServer.h>
+#include <ESP8266WiFi.h>
+#include <WiFiManager.h>
+#include <MQTT.h>
+#include <ArduinoJson.h>
+#include "secrets.h"
+
+// MQTT data
+#define MQTT_BUFFER_SIZE 1024             // the maximum size for packets being published and received
+MQTTClient mqttClient(MQTT_BUFFER_SIZE);  // handles the MQTT communication protocol
+WiFiClient networkClient;                 // handles the network connection to the MQTT broker
+#define MQTT_TOPIC_WELCOME "vigiloffice/welcome"
+String macAddress;
+
+const char *registerTopic;
+const char *statusTopic;
+const char *controlTopic;
+String settingsTopic;
+
+JsonDocument statusDoc;
+
+volatile bool applyChanges = false;
+
+void connectToMQTTBroker() {
+  static unsigned long lastDataSend = millis();
+  if (!mqttClient.connected()) {  // not connected
+    Serial.print(F("\nConnecting to MQTT broker..."));
+    while (!mqttClient.connect(MQTT_CLIENTID, MQTT_USERNAME, MQTT_PASSWORD)) {
+      Serial.print(F("."));
+      delay(1000);
+    }
+    Serial.println(F("\nConnected!"));
+
+    // connected to broker, subscribe topics
+    mqttClient.subscribe(MQTT_TOPIC_WELCOME);
+    Serial.println(F("\nSubscribed to welcome topic!"));
+  } else {
+    if (millis() - lastDataSend > 5000) {
+      statusDoc["indirizzo-mac"] = macAddress;
+      statusDoc["tipo"] = DEVICE_TYPE;
+      JsonArray sensori = statusDoc.createNestedArray("sensori");
+      JsonObject lightSensor = sensori.createNestedObject();
+      lightSensor["nome"] = "luce";
+      lightSensor["misurazione"] = lightValue;
+      lightSensor["stato"] = lightStatus;
+      lightSensor["abilitato"] = lightSensorStatus == LIGHT_SENSOR_ENABLED ? true : false;
+
+      JsonObject motionSensor = sensori.createNestedObject();
+      motionSensor["nome"] = "movimento";
+      motionSensor["misurazione"] = motionStatus == MOTION_INIT || motionStatus == MOTION_NORMAL ? 0 : 1;
+      motionSensor["stato"] = motionStatus;
+      motionSensor["abilitato"] = motionSensorStatus == MOTION_SENSOR_ENABLED ? true : false;
+
+      JsonObject flameSensor = sensori.createNestedObject();
+      flameSensor["nome"] = "fiamma";
+      flameSensor["misurazione"] = flameStatus == FLAME_PRESENT ? 1 : 0;
+      flameSensor["stato"] = flameStatus;
+      flameSensor["abilitato"] = flameSensorStatus == FLAME_SENSOR_ENABLED ? true : false;
+
+      JsonObject RGBLed = sensori.createNestedObject();
+      RGBLed["nome"] = "rgb";
+      RGBLed["misurazione"] = rgbStatus;
+      RGBLed["stato"] = rgbStatus;
+      RGBLed["abilitato"] = rgbSensorStatus == RGB_SENSOR_ENABLED ? true : false;
+
+
+      JsonObject alarm = statusDoc.createNestedObject("allarme");
+      alarm["stato"] = alarmStatus == ALARM_NORMAL ? false : true;
+      alarm["abilitato"] = alarmSystemStatus == ALARM_ENABLED ? true : false;
+
+      // Serialize JSON to a buffer
+      char buffer[1024];
+      size_t n = serializeJson(statusDoc, buffer);
+
+      // Print the serialized JSON
+      Serial.println(buffer);
+      mqttClient.publish(statusTopic, buffer, n, false, 1);
+      lastDataSend = millis();
+    }
+  }
+}
+
+void mqttMessageReceived(String &topic, String &payload) {
+  // this function handles a message from the MQTT broker
+  Serial.println("Incoming MQTT message: " + topic + " - " + payload);
+  if (topic == MQTT_TOPIC_WELCOME) {
+    JsonDocument doc;
+    deserializeJson(doc, payload);
+    registerTopic = doc["registerTopic"];
+    JsonDocument settingsDoc;
+    settingsDoc["indirizzo-mac"] = macAddress;
+    settingsDoc["tipo"] = DEVICE_TYPE;
+    char buffer[128];
+    size_t n = serializeJson(settingsDoc, buffer);
+    settingsTopic = registerTopic + String("/") + macAddress;
+    mqttClient.subscribe(settingsTopic);
+    mqttClient.publish(registerTopic, buffer, n);
+  } else if (topic == settingsTopic) {
+    JsonDocument doc;
+    deserializeJson(doc, payload);
+    statusTopic = doc["statusTopic"];
+    controlTopic = doc["controlTopic"];
+    mqttClient.subscribe(statusTopic);
+    mqttClient.subscribe(controlTopic);
+  } else if (topic == controlTopic) {
+  }
+}
+
+void commSetup() {
+  mqttClient.begin(MQTT_BROKERIP, 1883, networkClient);  // setup communication with MQTT broker
+  mqttClient.onMessage(mqttMessageReceived);             // callback on message received from MQTT broker
+  WiFiManager wifiManager;
+  //wifiManager.resetSettings();
+  wifiManager.autoConnect(AP_NAME);
+  macAddress = WiFi.macAddress();
+}
+
+void commLoop() {
+  connectToMQTTBroker();  // connect to MQTT broker (if not already connected)
+  mqttClient.loop();      // MQTT client loop
 }
 
 
@@ -229,14 +363,10 @@ void setup() {
   commSetup();
 }
 
-void applyChangesFromMaster(JsonDocument json){
-  Serial.println(F("applyChangesFromMaster called."));
-}
-
 void loop() {
   readFlame();
   readLight();
   readMotion();
   updateRGB();
-  commLoop(applyChangesFromMaster);
+  commLoop();
 }
