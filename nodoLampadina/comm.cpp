581fc6e1b8dde8be0b0ad907329259c2383294a7
Alessandro Maifredi <a.maifredi@campus.unimib.it>
2024-05-24 15:45:53 +0200

    communication layer

--- nodoLampadina/comm.cpp
+++ nodoLampadina/comm.cpp
@@ -1,3 +1,4 @@
+#include "wiring_private.h"
 #include <DNSServer.h>
 #include <ESP8266WiFi.h>
 #include <WiFiManager.h>
@@ -6,14 +7,23 @@
 #include "secrets.h"
 
 // MQTT data
-#define MQTT_BUFFER_SIZE 128               // the maximum size for packets being published and received
-MQTTClient mqttClient(MQTT_BUFFER_SIZE);   // handles the MQTT communication protocol
-WiFiClient networkClient;                  // handles the network connection to the MQTT broker
+#define MQTT_BUFFER_SIZE 512              // the maximum size for packets being published and received
+MQTTClient mqttClient(MQTT_BUFFER_SIZE);  // handles the MQTT communication protocol
+WiFiClient networkClient;                 // handles the network connection to the MQTT broker
 #define MQTT_TOPIC_WELCOME "vigiloffice/welcome"
-#define AP_NAME "VigilOffice-Lampadina"
+
+const char *registerTopic;
+const char *statusTopic;
+const char *controlTopic;
+const char *settingsTopic;
+
+JsonDocument statusDoc;
+
+volatile bool applyChanges = false;
 
 void connectToMQTTBroker() {
-  if (!mqttClient.connected()) {   // not connected
+  static unsigned long lastDataSend = millis();
+  if (!mqttClient.connected()) {  // not connected
     Serial.print(F("\nConnecting to MQTT broker..."));
     while (!mqttClient.connect(MQTT_CLIENTID, MQTT_USERNAME, MQTT_PASSWORD)) {
       Serial.print(F("."));
@@ -24,6 +34,11 @@ void connectToMQTTBroker() {
     // connected to broker, subscribe topics
     mqttClient.subscribe(MQTT_TOPIC_WELCOME);
     Serial.println(F("\nSubscribed to welcome topic!"));
+  } else {
+    if (millis() - lastDataSend > 5000) {
+      //update json with current status
+      //publish on statusTopic
+    }
   }
 }
 
@@ -33,21 +48,46 @@ void mqttMessageReceived(String &topic, String &payload) {
   if (topic == MQTT_TOPIC_WELCOME) {
     JsonDocument doc;
     deserializeJson(doc, payload);
-    const char *serverIP = doc["serverIP"];
-    Serial.println(serverIP);
+    registerTopic = doc["registerTopic"];
+    JsonDocument settingsDoc;
+    doc["macAddress"] = "mac";
+    doc["type"] = DEVICE_TYPE;
+    char buffer[128];
+    size_t n = serializeJson(settingsDoc, buffer);
+    String newSettingsTopic = registerTopic + "mac";
+    settingsTopic = newSettingsTopic.c_str();
+    mqttClient.subscribe(settingsTopic);
+    mqttClient.publish(registerTopic, buffer, n);
+
+  } else if (topic == settingsTopic) {
+    JsonDocument doc;
+    deserializeJson(doc, payload);
+    statusTopic = doc["statusTopic"];
+    controlTopic = doc["controlTopic"];
+    mqttClient.subscribe(statusTopic);
+    mqttClient.subscribe(controlTopic);
+  } else if (topic == controlTopic) {
+    //deserialize status json
+    //update status json
+    //set applyChanges to true
+    applyChanges = true;
   }
 }
 
+
 void commSetup() {
-  mqttClient.begin(MQTT_BROKERIP, 1883, networkClient);   // setup communication with MQTT broker
-  mqttClient.onMessage(mqttMessageReceived);              // callback on message received from MQTT broker
+  mqttClient.begin(MQTT_BROKERIP, 1883, networkClient);  // setup communication with MQTT broker
+  mqttClient.onMessage(mqttMessageReceived);             // callback on message received from MQTT broker
   WiFiManager wifiManager;
   //wifiManager.resetSettings();
   wifiManager.autoConnect(AP_NAME);
 }
 
-void commLoop() {
-  connectToMQTTBroker();   // connect to MQTT broker (if not already connected)
-  mqttClient.loop();       // MQTT client loop
+void commLoop(std::function<void(JsonDocument)> callback) {
+  connectToMQTTBroker();  // connect to MQTT broker (if not already connected)
+  mqttClient.loop();      // MQTT client loop
+  if (applyChanges) {
+    callback(statusDoc);
+    applyChanges = false;
+  }
 }
-
--- nodoLampadina/secrets.h
+++ nodoLampadina/secrets.h
@@ -1,4 +1,6 @@
 #define MQTT_BROKERIP "149.132.178.179"         // IP address of the machine running the MQTT broker
-#define MQTT_CLIENTID "VigilOffice-Lampadina-bc:dd:c2:b6:6f:9b"                 // client identifier
-#define MQTT_USERNAME "QazimToska"         // mqtt user's name
-#define MQTT_PASSWORD "iot847361"         // mqtt user's password
+#define MQTT_CLIENTID "VO-L-bc:dd:c2:b6:6f:9b"                 // client identifier
+#define MQTT_USERNAME "AlessandroMaifredi"         // mqtt user's name
+#define MQTT_PASSWORD "iot851610"         // mqtt user's password
+#define DEVICE_TYPE "LAMPADINA"
+#define AP_NAME "VigilOffice-Lampadina"
