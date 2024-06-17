31dd294f38fd093dfe8bb02b17e52dc37ec8e2f6
Alessandro Maifredi <a.maifredi@campus.unimib.it>
2024-06-17 10:26:40 +0200

    mac fix

--- parkingNode/secrets.h
+++ parkingNode/secrets.h
@@ -1,5 +1,5 @@
 #define MQTT_BROKERIP "149.132.178.179"        // IP address of the machine running the MQTT broker
-#define MQTT_CLIENTID "VO-P-11:22:33:44:55:66" // client identifier
+#define MQTT_CLIENTID "VO-P-84:f3:eb:53:5f:b0" // client identifier
 #define MQTT_USERNAME "AlessandroMaifredi"     // mqtt user's name
 #define MQTT_PASSWORD "iot851610"              // mqtt user's password
 #define DEVICE_TYPE "parking"
